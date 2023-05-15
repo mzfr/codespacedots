44527c42ccf7d3fffc0171ceb0fb45a434585337
Mehtab zafar <mehtab.zafar98@gmail.com>
2023-05-15 07:49:48 +0000

    Some stuff

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 # !/bin/bash
 
-#sudo apt update && sudo apt -y upgrade
 
 #Install zsh
-#sudo apt install zsh
+sudo apt install zsh
 
 #Install lazygit
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Linux_x86_64.tar.gz
 tar -xvf lazygit_0.38.2_Linux_x86_64.tar.gz
 sudo mv lazygit /usr/local/bin
 rm LICENSE README.md
+
+# Fira Code Installation
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/0fc23725fb88e27c01cfd1540bd6151de9ee528d/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf
 
 # Install Neovim latest version
 sudo apt-get install ninja-build gettext cmake unzip curl
@@ -20,5 +23,6 @@ sudo make install
 #Install lunarvim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
+# Install ohmyzsh and powerlevel10k theme
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
