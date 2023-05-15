a8171be51c8c4fdae491f01a7a459768e55394a8
Mehtab zafar <mehtab.zafar98@gmail.com>
2023-05-15 07:46:12 +0000

    Final

--- setup.sh
+++ setup.sh
@@ -5,15 +5,20 @@
 #Install zsh
 #sudo apt install zsh
 
+#Install lazygit
+wget https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Linux_x86_64.tar.gz
+tar -xvf lazygit_0.38.2_Linux_x86_64.tar.gz
+sudo mv lazygit /usr/local/bin
+rm LICENSE README.md
+
 # Install Neovim latest version
 sudo apt-get install ninja-build gettext cmake unzip curl
 git clone https://github.com/neovim/neovim
-git checkout stable
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd neovim && git checkout stable &&make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install 
 
 #Install lunarvim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
