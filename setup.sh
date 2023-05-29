d220d4e5f817589122461bf61bd2875c5b29b7af
Mehtab zafar <mehtab.zafar98@gmail.com>
2023-05-29 08:36:37 +0000

    Few more things

--- setup.sh
+++ setup.sh
@@ -12,8 +12,11 @@ sudo apt install helix
 mkdir ~/.config/helix && mkdir ~/.config/helix/themes
 wget https://gitlab.com/0xmzfr/mydots/-/raw/main/onedark.toml -O ~/.config/helix/themes/onedark.toml
 
-# Install pyright and get helix config files
+# Install LSP's
 npm install --location=global pyright
+npm i -g vscode-langservers-extracted
+
+# Setting helix config
 wget https://gitlab.com/0xmzfr/mydots/-/raw/main/config.toml -O ~/.config/helix/config.toml
 wget https://gitlab.com/0xmzfr/mydots/-/raw/main/languages.toml -O ~/.config/helix/languages.toml
 
@@ -21,6 +24,11 @@ wget https://gitlab.com/0xmzfr/mydots/-/raw/main/languages.toml -O ~/.config/hel
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Linux_x86_64.tar.gz
 tar -xvf lazygit_0.38.2_Linux_x86_64.tar.gz
 sudo mv lazygit /usr/local/bin
+
+# Install lf
+wget https://github.com/gokcehan/lf/releases/download/r30/lf-linux-amd64.tar.gz
+tar -xvf lf-linux-amd64.tar.gz
+sudo mv ./lf /usr/local/bin
 
 # Install ohmyzsh and powerlevel10k theme
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
